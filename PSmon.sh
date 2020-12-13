ebfcc339a359618e1fcaff87931bebafc66a9e00
Miguel R <d4t4s3c@protonmail.com>
2020-12-13 22:03:18 +0100

    fix

--- PSmon.sh
+++ PSmon.sh
@@ -8,6 +8,6 @@ old=$(ps -eo command)
 
 while true; do
         new=$(ps -eo command)
-        diff <(echo "$old") <(echo "$new") | grep "[\>\<]" | grep -v -E "kworker|command"
+        diff <(echo "$old") <(echo "$new") | grep "[\>\<]" | grep -v -E "Psmon|kworker|command"
         old=$new
 done
